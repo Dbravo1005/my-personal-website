3a02fafb8e3542998e394f23c781b2522c7b94ff
Damian Bravo <Dbravo1005@gmail.com>
2023-04-08 11:57:21 -0400

    add script.js, connect to index.html

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 const h2 = document.createElement("h2");
-h2.textContent = "This content added by javascipt";
+h2.textContent = "Who is Damian V Bravo?";
 document.querySelector("body").appendChild(h2);
 
+const h4 = document.createElement("h4");
+h4.textContent = "Damian was in New York being Half-Mexican and Half-Ecuadorian!";
+document.querySelector("body").appendChild(h4);
 
-const h3 = document.createElement("h3");
-h3.textContent = "We made it!";
-document.querySelector("body").appendChild(h3);
+const h5 = document.createElement("h5");
+h5.textContent = "Damian loves to spend time with his family (who live in Ohio) and loves to spend time with his friends.";
+document.querySelector("body").appendChild(h5);
